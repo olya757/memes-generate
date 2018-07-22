ddc991a9034034984af57d1cea21aeabba978263
Дима Трофимов <dmitrijtrofimov@macbook-pro-dmitrij.local>
2018-07-22 23:07:28 +0300

    Initial Commit

---
+++ memes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  memes
+//
+//  Created by Дмитрий Трофимов on 22.07.2018.
+//  Copyright © 2018 Ольга Шишкина. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
