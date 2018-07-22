16f1700ddd0ad782e63777cb8c714205e5e5e8d0
Дима Трофимов <dmitrijtrofimov@macbook-pro-dmitrij.local>
2018-07-23 00:47:56 +0300

    commit2

--- memes/ViewController.swift
+++ memes/ViewController.swift
@@ -8,22 +8,74 @@
 
 import UIKit
 
+class Pair{
+    var image = UIImage(named : "gendalf.jpg")
+    var text = ""
+    init(i: UIImage, t : String){
+        text = t
+        image = i
+    }
+}
+
 class ViewController: UIViewController , UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        var i : Int = 0
+        while (i<text.count){
+            objects.append(Pair(i: images[i]!,t: text[i]))
+            i=i+1
+        }
+        filtred=objects
         // Do any additional setup after loading the view, typically from a nib.
+        collectionView.dataSource=self
+        collectionView.delegate = self
+        collectionView.register(UINib.init(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CollectionViewCell")
+        
+        searchBar.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    var objects = [Pair]()
+    var filtred = [Pair]()
+    var text = ["item1","i2","i3","i4"]
+    
+    var images = [UIImage(named: "gendalf.jpg"),UIImage(named: "putin.jpg"),UIImage(named: "benladon.png"),UIImage(named: "image1.jpg")]
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     @IBOutlet weak var searchBar: UISearchBar!
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return filtred.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        cell.ivImage.image=filtred[indexPath.item].image
+        cell.tvText.text = filtred[indexPath.item].text
+        
+        
+        
+        cell.ivImage.frame = CGRect(x: 0, y: 0, width: Int(self.view.frame.size.width)/2, height: 150)
+        cell.tvText.frame = CGRect(x: Int(self.view.frame.size.width)/2, y: 0, width: Int(self.view.frame.size.width)/2, height: 150)
+        return cell
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.reloadData(str: searchText)
+    }
+    
+    func reloadData(str : String){
+        if (str==""){
+            filtred = objects
+            collectionView.reloadData()
+            return
+        }
+        filtred = objects.filter{
+            item in return item.text.lowercased().contains(str.lowercased())
+        }
+        collectionView.reloadData()
+    }
     
 }
 
